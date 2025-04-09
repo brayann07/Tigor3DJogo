9e2ec71443c9928bbceb78f97cb34a0c992cb269
Brayan Freitas Dierings <brayanloopy@gmail.com>
2025-04-08 22:40:01 -0300

    tiger e terreno

---
+++ ProjetoTiger3D/Assets/Personagens/Scripts/TigerMov.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class TigerMov : MonoBehaviour
+{
+    public float Speed;
+    public float RotSpeed;
+    private float Rotation;
+    public float Gravity;
+
+    Vector3 MoveDirection;
+    CharacterController controller;
+    Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (controller.isGrounded)
+        {
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                MoveDirection = Vector3.forward * Speed;
+                MoveDirection = transform.TransformDirection(MoveDirection);
+                anim.SetInteger("transitions", 1);
+            }
+         
+            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+            {
+                MoveDirection = Vector3.forward * (3.0f * Speed);
+                MoveDirection = transform.TransformDirection(MoveDirection);
+                anim.SetInteger("transitions", 2);
+            }
+            if (Input.GetKeyUp(KeyCode.W) && Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                MoveDirection = Vector3.zero;
+                anim.SetInteger("transitions", 0);
+                
+            }
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                MoveDirection = Vector3.zero;
+                anim.SetInteger("transitions", 0);
+              
+            }
+        }
+        Rotation += Input.GetAxis("Horizontal") * RotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, Rotation, 0);
+        if(Rotation > 0)
+        {
+            anim.SetInteger("transitions", 1);
+        }
+        if(Rotation < 0)
+        {
+            anim.SetInteger("transitions", 0);
+        }
+
+        MoveDirection.y -= Gravity * Time.deltaTime;
+        controller.Move(MoveDirection * Time.deltaTime);
+    }
+
+}
